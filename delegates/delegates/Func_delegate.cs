493b3ab4cd18b9b7d24a5b2ca961fd9a8dbe97f2
Tushar <dhanorkartushar2@gmail.com>
2023-07-24 14:59:24 +0530

    .

---
+++ delegates/delegates/Func_delegate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates
+{
+    internal class Func_delegate
+    {
+        static void Main(string[] args)
+        {
+            Math_operations mo=new Math_operations();
+            Func<int, int, int> f = mo.Add;
+            int res = f(9,1);
+            Console.WriteLine(res);
+
+            Func<int, int, int> f1 = mo.Sub;
+            int res2 = f1(9,1);
+            Console.WriteLine(res2);
+
+        }
+    }
+
+    class Math_operations 
+    {
+        public int Add(int a,int b)
+        {
+            Console.WriteLine("In Add:");
+            return a + b;
+        }
+
+        public int Sub(int a,int b) 
+        {
+            Console.WriteLine("Substraction:");
+            return a - b;
+        }
+    }
+}
---
+++ delegates/delegates/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace delegates
+{
+    
+    public delegate void Mydel(int a);
+
+    class Calculation
+    { 
+        public static void Display(int s)
+        {
+            Console.WriteLine(s);
+        }
+        public static void Square( int s)
+        { 
+            Console.WriteLine(s*s);
+        }
+
+        public static void Cube(int s) 
+        {
+            Console.WriteLine(s*s*s);
+        }
+    }
+
+    class Calc
+    {
+        public static void Multi(int n)
+        {
+            Console.WriteLine(10*n);
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("here we are calling methods by delegates");
+            //creating object/instance of delegates 
+            //d.Invoke(5); or
+          
+
+            //Calculation c = new Calculation();
+            Mydel d1=new Mydel(Calculation.Square);
+            d1.Invoke(9);
+
+            Mydel d=new Mydel(Calculation.Cube);
+            d.Invoke(10);
+
+        }
+    }
+}
